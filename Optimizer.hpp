59e2d4742f5b0a92b514599ad48134fdbfe1bf0f
Dejun Lin <dejun@cole.biophysics.rochester.edu>
2014-05-29 14:27:05 -0400

    add interface to LOOS::Simplex optimizer

--- Optimizer.hpp
+++ Optimizer.hpp
@@ -18,6 +18,7 @@
 #include "dlib/optimization.h"
 #include <algorithm>
 #include "typedefs.hpp"
+#include "/home/dejun/dejun-loos-branch/Simplex.hpp"
 
 namespace dlibOPT {
 /*
@@ -118,19 +119,93 @@ class gradFunct
 template<class Kernel, 
 	 class iT
         >
-void optimize_dlib_lbfgs(Kernel& funct, iT& startingpoint, const double& tol) {
+iT optimize_dlib_lbfgs(Kernel& funct, iT& startingpoint, const double& tol) {
   typedef dlib::matrix<double,0,1> cvect;
   Funct<Kernel, iT, cvect, double> ft(&funct);
   gradFunct<Kernel, iT, cvect, cvect> gradft(&funct);
   cvect sp(startingpoint.size());
+  cvect lowbound(startingpoint.size());
+  cvect upbound(startingpoint.size());
   for(uint i = 0; i < startingpoint.size(); ++i) {
-    sp(i) = startingpoint[i]; 
+    sp(i) = startingpoint[i];
+    lowbound(i) = -500;
+    upbound(i) = 500;
   }
   dlib::find_min(dlib::lbfgs_search_strategy(10),  // The 10 here is basically a measure of how much memory L-BFGS will use.
            dlib::objective_delta_stop_strategy(tol).be_verbose(),  // Adding be_verbose() causes a message to be 
                                                                     // printed for each iteration of optimization.
-            ft, gradft, sp, -1);
+            ft, gradft, sp, -1e7);
+  /*dlib::find_min_box_constrained(dlib::lbfgs_search_strategy(10),  // The 10 here is basically a measure of how much memory L-BFGS will use.
+           dlib::objective_delta_stop_strategy(tol).be_verbose(),  // Adding be_verbose() causes a message to be 
+                                                                    // printed for each iteration of optimization.
+            ft, gradft, sp, lowbound, upbound);*/
+  for(uint i = 0; i < startingpoint.size(); ++i) {
+    startingpoint[i] = sp(i);
+  }
+  return startingpoint;
 }
 
 }
 
+namespace SimplexOPT {
+/*
+ * =====================================================================================
+ *        Class:  Funct
+ *  Description:  Wrapper to kernel which calculates the value of the function
+ *  NOTE that this should be used in conjunction with gradFunct since it requires
+ *  Funct and gradFunct are called in pair such that Funct is called first and then 
+ *  is gradFunct. The Kernel::operator()() is the function that calculates both 
+ *  function value and gradient, which are stored as member variables of Kernel
+ * =====================================================================================
+ */
+template < class Kernel,                        /* The kernel that actually do the calculation */
+	   class iKT,                           /*  Input type for Kernel::operator() */
+	   class iT,                            /*  Input type of argument to whatever optimziation routine*/
+	   class oT                             /*  Output type of argument to whatever optimziation routine*/
+	 >
+class Funct
+{
+  public:
+    /* ====================  LIFECYCLE     ======================================= */
+    Funct (Kernel* const _pKernel) : pKernel(_pKernel) {};                             /* constructor */
+
+    /* ====================  ACCESSORS     ======================================= */
+
+    /* ====================  MUTATORS      ======================================= */
+
+    /* ====================  OPERATORS     ======================================= */
+    oT operator()(const iT& x) const {
+      const iKT ikx(x.begin(), x.end());
+      pKernel->operator()(ikx);
+      return pKernel->getF();
+    };
+
+  protected:
+    /* ====================  METHODS       ======================================= */
+
+    /* ====================  DATA MEMBERS  ======================================= */
+
+  private:
+    /* ====================  METHODS       ======================================= */
+
+    /* ====================  DATA MEMBERS  ======================================= */
+    Kernel* const pKernel;
+
+}; /* ----------  end of template class Funct  ---------- */
+
+template<class Kernel>
+vector<valtype> optimize_Simplex(Kernel& funct, vector<valtype>& seeds, const double& tol, const uint& maxit)
+{
+  const uint ndim = seeds.size();
+  Simplex<valtype> optimizer(ndim);
+  optimizer.tolerance(tol);
+  optimizer.maximumIterations(maxit);
+
+  vector<valtype> ini_stepsize(ndim, 10);
+  optimizer.seedLengths(ini_stepsize);
+
+  Funct<Kernel, vector<valtype>, vector<valtype>, valtype> F(&funct);
+  return optimizer.optimize(seeds, F); 
+}
+
+}
