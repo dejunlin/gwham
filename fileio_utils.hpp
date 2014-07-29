5e8cd4628e84dc0a6cac3101ae48a8e342a0fefc
Dejun Lin <dejun@cole.biophysics.rochester.edu>
2014-07-28 20:43:53 -0400

    add some fileio util functions

--- fileio_utils.hpp
+++ fileio_utils.hpp
@@ -5,11 +5,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include "exception.hpp"
 
 using namespace std;
 
 template <class Tp> 
-void parser(vector<Tp>& output, const string& str, string delims=" \t") {
+void parser(vector<Tp>& output, const string& str, string delims=" \t") throw(FILEIO_Exception) {
 	char * str_c = new char[str.size()+1];
 	str_c[str.size()] = '\0';
 	memcpy(str_c,str.c_str(),str.size());
@@ -19,8 +22,8 @@ void parser(vector<Tp>& output, const string& str, string delims=" \t") {
 		istringstream iss(pch);
 		Tp tok;
 		if(!(iss>>tok)) {
-			cout << "# Error parsing string!" << endl;
-			exit(-1);
+		  delete[] str_c;
+		  throw(FILEIO_Exception("Error parsing string: '" + str + "'"));
 		}
 		output.push_back(tok);
 		pch = strtok(NULL,delims.c_str());
@@ -34,4 +37,39 @@ void parser(vector<Tp>& output, const vector<string>& input) {
     output.push_back(atof(input[i].c_str()));
   }
 }
+
+template <class T>
+string tostr(const T& input) {
+  stringstream ss;
+  ss << input;
+  return ss.str();
+}
+
+//case-sensitive comparison; NOTE std::string::compare() return 0 if and only if the two string match
+template <class T>
+bool matchkey(const string& entry, const T& key) {
+  const string& keystr = tostr(key);
+  return entry.compare(keystr) ? false : true; 
+};
+
+//case-insensitive comparison
+template <class T>
+bool cmatchkey(const string& entry, const T& key) {
+  const string& keystr = tostr(key);
+  return strcasecmp(entry.c_str(), keystr.c_str()) ? false : true; 
+};
+
+template <class T>
+void setsc(const string& entry, T& output) {
+  vector<T> outvec;
+  parser(outvec, entry);
+  output = outvec[0]; 
+};
+
+template <class T>
+void setvec(const string& entry, vector<T>& output) throw(FILEIO_Exception) { 
+  try { parser(output, entry); } 
+  catch(FILEIO_Exception& fioex) { throw fioex; } 
+};
+
 #endif
