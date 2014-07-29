c9846c0c64b0bbaee5d594ac240d181f3d25c6fd
Dejun Lin <dejun@cole.biophysics.rochester.edu>
2014-07-29 17:13:43 -0400

    a lot of changes; first, add print() function to print out mdp parameters; second, always use nocmmatchkey() calls instead of matchkey(); third, add PULLCNTGRP to handle contact pulling

--- gmxmdp.hpp
+++ gmxmdp.hpp
@@ -39,6 +39,7 @@ class GMXMDP
     GMXMDP (const string& fname);    /* constructor */
 
     /* ====================  ACCESSORS     ======================================= */
+    void print() const; /*  print out all the parameters read */
 
     /* ====================  MUTATORS      ======================================= */
 
@@ -61,6 +62,7 @@ class GMXMDP
 	FEP () ;                             /* constructor */
 
 	/* ====================  ACCESSORS     ======================================= */
+        void print() const; /*  print out all the parameters read */
 
 	/* ====================  MUTATORS      ======================================= */
 
@@ -99,6 +101,7 @@ class GMXMDP
 	PULL ();                             /* constructor */
 
 	/* ====================  ACCESSORS     ======================================= */
+        void print() const; /*  print out all the parameters read */
 
 	/* ====================  MUTATORS      ======================================= */
 
@@ -106,11 +109,13 @@ class GMXMDP
 	//parse the mdp options for pull
         bool operator()(const string& opt, const string& optval) throw(GMXMDP_Exception, FILEIO_Exception);
 	/* ====================  DATA MEMBERS  ======================================= */
-	enum PullType {Umbrella, Constraint, ConstantForce, NPullTypes} pullT;
+	enum PullType {Umbrella, Constraint, ConstantForce, Contact, NPullTypes} pullT;
 	enum PullGeom {Distance, Direction, DirectionPeriodic, Cylinder, Position, NGeomTypes} geomT;
+	//bit mask of the pulled dimension in left-to-right order (e.g., 001 <=> Y N N, meaning only x dimention is pulled)
 	short dim;
 	int nstx, nstf;
 	int npgrps;
+	int ncntgrps;
 	/*
 	 * =====================================================================================
 	 *        Class:  PULLGRP
@@ -124,6 +129,7 @@ class GMXMDP
 	    PULLGRP ();                             /* constructor */
 
 	    /* ====================  ACCESSORS     ======================================= */
+            void print() const; /*  print out all the parameters read */
 
 	    /* ====================  MUTATORS      ======================================= */
 
@@ -145,6 +151,41 @@ class GMXMDP
 
 	}; /* -----  end of class PULLGRP  ----- */
 	vector<PULLGRP> pullgrps;
+
+	/*
+	 * =====================================================================================
+	 *        Class:  PULLCNTGRP
+	 *  Description:  contact-group parameters
+	 * =====================================================================================
+	 */
+	class PULLCNTGRP
+	{
+	  public:
+	    /* ====================  LIFECYCLE     ======================================= */
+	    PULLCNTGRP ();                             /* constructor */
+
+	    /* ====================  ACCESSORS     ======================================= */
+            void print() const; /*  print out all the parameters read */
+
+	    /* ====================  MUTATORS      ======================================= */
+
+	    /* ====================  OPERATORS     ======================================= */
+	    //parse the mdp options for contact-group
+            bool operator()(const string& opt, const string& optval, const string& i) throw(GMXMDP_Exception, FILEIO_Exception);
+
+	  protected:
+	    /* ====================  METHODS       ======================================= */
+
+	    /* ====================  DATA MEMBERS  ======================================= */
+	    double rate, nc, ncB, k, kB;
+
+	  private:
+	    /* ====================  METHODS       ======================================= */
+
+	    /* ====================  DATA MEMBERS  ======================================= */
+
+	}; /* -----  end of class PULLCNTGRP  ----- */
+	vector<PULLCNTGRP> pullcntgrps;
 
 
       protected:
@@ -176,14 +217,11 @@ GMXMDP<THamiltonian>::GMXMDP(const string& fname):
   fep(FEP()),
   pull(PULL())
 {
-  fileio fio(fname, fstream::in, 1, 0, 1, MAXNLINE);
+  fileio fio(fname, fstream::in, 1, 0, 1, MAXNLINE, ";");
   while(fio.readaline()) {
-    cout << "Reading line " << fio.line << endl;
     const vector<string> tmpstr = fio.line2str("=");
-    if(!tmpstr.size()) { continue; } //skip empty lines
-    const string& opt = tmpstr[0];
-    const string& optval = tmpstr[1];
-    cout << opt << " ===== " << optval << endl;
+    const string& opt = trimltcm(tmpstr[0], ";", " \t");
+    const string& optval = trimltcm(tmpstr[1], ";", " \t");
     try {
       const bool p = fep(opt, optval) || pull(opt, optval);
     } catch(GMXMDP_Exception& gmxmdpex) {
@@ -194,6 +232,12 @@ GMXMDP<THamiltonian>::GMXMDP(const string& fname):
       terminate();
     }
   }
+}
+
+template < class THamiltonian >
+void GMXMDP<THamiltonian>::print() const {
+  fep.print();
+  pull.print();
 }
 
 template < class THamiltonian >
@@ -213,37 +257,66 @@ GMXMDP<THamiltonian>::FEP::FEP() :
 }
 
 template < class THamiltonian >
+void GMXMDP<THamiltonian>::FEP::print() const {
+  printf("#%20s = %-5d\n", "free-energy", fepT);
+  printf("#%20s = ", "bonded-lambdas");
+  copy(Lbond.begin(), Lbond.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#%20s = ", "mass-lambdas");
+  copy(Lmass.begin(), Lmass.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#%20s = ", "vdw-lambdas");
+  copy(Lvdw.begin(), Lvdw.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#%20s = ", "coul-lambdas");
+  copy(Lcoul.begin(), Lcoul.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#%20s = ", "rst-lambdas");
+  copy(Lrst.begin(), Lrst.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#%20s = ", "cnt1-lambdas");
+  copy(Lcnt1.begin(), Lcnt1.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#%20s = ", "cnt2-lambdas");
+  copy(Lcnt2.begin(), Lcnt2.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#%20s = ", "cnt3-lambdas");
+  copy(Lcnt3.begin(), Lcnt3.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+}
+
+template < class THamiltonian >
 bool GMXMDP<THamiltonian>::FEP::operator()(const string& opt, const string& optval) throw(GMXMDP_Exception, FILEIO_Exception)
 {
-  if(matchkey(opt, "free-energy")) {
-    if(matchkey(optval,"yes")) {
+  if(nocmmatchkey(opt, "free-energy")) {
+    if(nocmmatchkey(optval,"yes")) {
 	fepT = Yes; 
-    } else if(matchkey(optval, "expanded")) {
+    } else if(nocmmatchkey(optval, "expanded")) {
 	fepT = Expanded;
     } else {
       throw(GMXMDP_Exception("Unknown FEP type: " + optval));
     }
-  } else if(matchkey(opt, "bonded-lambdas")) {
+  } else if(nocmmatchkey(opt, "bonded-lambdas")) {
     setvec(optval, Lbond);
-  } else if(matchkey(opt, "mass-lambdas")) {
+  } else if(nocmmatchkey(opt, "mass-lambdas")) {
     setvec(optval, Lmass);
-  } else if(matchkey(opt, "vdw-lambdas")) {
+  } else if(nocmmatchkey(opt, "vdw-lambdas")) {
     setvec(optval, Lvdw);
-  } else if(matchkey(opt, "coul-lambdas")) {
+  } else if(nocmmatchkey(opt, "coul-lambdas")) {
     setvec(optval, Lcoul);
-  } else if(matchkey(opt, "restraint-lambdas")) {
+  } else if(nocmmatchkey(opt, "restraint-lambdas")) {
     setvec(optval, Lrst);
-  } else if(matchkey(opt, "umbrella-contact1-lambdas")) {
+  } else if(nocmmatchkey(opt, "umbrella-contact1-lambdas")) {
     setvec(optval, Lcnt1);
-  } else if(matchkey(opt, "umbrella-contact2-lambdas")) {
+  } else if(nocmmatchkey(opt, "umbrella-contact2-lambdas")) {
     setvec(optval, Lcnt2);
-  } else if(matchkey(opt, "umbrella-contact3-lambdas")) {
+  } else if(nocmmatchkey(opt, "umbrella-contact3-lambdas")) {
     setvec(optval, Lcnt3);
-  } else if(matchkey(opt, "init-lambda-state")) {
+  } else if(nocmmatchkey(opt, "init-lambda-state")) {
     setsc(optval, Linit);
-  } else if(matchkey(opt, "nstdhdl")) {
+  } else if(nocmmatchkey(opt, "nstdhdl")) {
     setsc(optval, nstdhdl);
-  } else { 
+  } else {
     return false;
   }
   return true;
@@ -257,65 +330,100 @@ GMXMDP<THamiltonian>::PULL::PULL() :
   nstx(-1),
   nstf(-1),
   npgrps(0),
-  pullgrps(vector<PULLGRP>(npgrps, PULLGRP()))
+  ncntgrps(0),
+  pullgrps(vector<PULLGRP>(npgrps, PULLGRP())),
+  pullcntgrps(vector<PULLCNTGRP>(ncntgrps, PULLCNTGRP()))
 {
+}
+
+template < class THamiltonian >
+void GMXMDP<THamiltonian>::PULL::print() const {
+  printf("#%20s = %-5d\n", "Pull", pullT);
+  printf("#%20s = %-5d\n", "Pull-geometry", geomT);
+  printf("#%20s = %-5d\n", "Pull-dim", dim);
+  printf("#%20s = %-5d\n", "Pull-nstxout", nstx);
+  printf("#%20s = %-5d\n", "Pull-nstfout", nstf);
+  printf("#%20s = %-5d\n", "Pull-ngroups", npgrps);
+  printf("#%20s = %-5d\n", "Pull-ncontactgroups", ncntgrps);
+  if(pullT == Contact) {
+    for(uint i = 0; i < ncntgrps; ++i) {
+      printf("#%20s%-5d\n", "Pull-contactgroup", i);
+      pullcntgrps[i].print();
+    }
+  } else {
+    for(uint i = 0; i < npgrps; ++i) {
+      printf("#%20s%-5d\n", "Pullgroup", i+1);
+      pullgrps[i].print();
+    }
+  }
 }
 
 template < class THamiltonian >
 bool GMXMDP<THamiltonian>::PULL::operator()(const string& opt, const string& optval) throw(GMXMDP_Exception, FILEIO_Exception)
 {
-  if(matchkey(opt, "pull")) {
-    if(matchkey(optval,"umbrella")) {
+  if(nocmmatchkey(opt, "pull")) {
+    if(nocmmatchkey(optval,"umbrella")) {
       pullT = Umbrella; 
-    } else if(matchkey(optval, "constraint")) {
+    } else if(nocmmatchkey(optval, "constraint")) {
       pullT = Constraint;
-    } else if(matchkey(optval, "constant-force")) {
+    } else if(nocmmatchkey(optval, "constant-force")) {
       pullT = ConstantForce;
+    } else if(nocmmatchkey(optval, "contact")) {
+      pullT = Contact;
     } else {
-      throw(GMXMDP_Exception("Unknown Pull type: " + optval));
+      throw(GMXMDP_Exception("Unknown Pull type: '" + optval + "'"));
     }
-  } else if(matchkey(opt, "pull_geometry") || matchkey(opt, "pull-geometry")) {
-    if(matchkey(optval,"distance")) {
+  } else if(nocmmatchkey(opt, "pull_geometry") || nocmmatchkey(opt, "pull-geometry")) {
+    if(nocmmatchkey(optval,"distance")) {
       geomT = Distance; 
-    } else if(matchkey(optval, "direction")) {
+    } else if(nocmmatchkey(optval, "direction")) {
       geomT = Direction; 
-    } else if(matchkey(optval, "direction-periodic")) {
+    } else if(nocmmatchkey(optval, "direction-periodic")) {
       geomT = DirectionPeriodic; 
-    } else if(matchkey(optval, "cylinder")) {
+    } else if(nocmmatchkey(optval, "cylinder")) {
       geomT = Cylinder; 
-    } else if(matchkey(optval, "position")) {
+    } else if(nocmmatchkey(optval, "position")) {
       geomT = Position; 
     } else {
-      throw(GMXMDP_Exception("Unknown Pull-geometry type: " + optval));
+      throw(GMXMDP_Exception("Unknown Pull-geometry type: '" + optval + "'"));
     }
-  } else if(matchkey(opt, "pull_dim") || matchkey(opt, "pull-dim")) {
+  } else if(nocmmatchkey(opt, "pull_dim") || nocmmatchkey(opt, "pull-dim")) {
     vector<string> dims;
     setvec(optval, dims);
     if(dims.size() != 3) { throw(GMXMDP_Exception("pull dimension must be <= 3 but we got: " + tostr(dims.size()))); }
     for(uint i = 0; i < 3; ++i) {
-      if(cmatchkey(dims[i], "Y")) {
+      if(nocmcmatchkey(dims[i], "Y")) {
 	dim |= 1 << i;
-      } else if(cmatchkey(dims[i], "N")) {
+      } else if(nocmcmatchkey(dims[i], "N")) {
 	const short mask = numeric_limits<short>::max() ^ (1 << i) ;
-	dim |= mask;
+	dim &= mask;
       } else {
         throw(GMXMDP_Exception("Unknown Pull-dim: " + optval));
       }
     }
-  } else if(matchkey(opt, "pull_nstxout") || matchkey(opt, "pull-nstxout")) {
+  } else if(nocmmatchkey(opt, "pull_nstxout") || nocmmatchkey(opt, "pull-nstxout")) {
     setsc(optval, nstx);
-  } else if(matchkey(opt, "pull_start") || matchkey(opt, "pull-start")) {
+  } else if(nocmmatchkey(opt, "pull_start") || nocmmatchkey(opt, "pull-start")) {
     return true; //not really need this but just to avoid additional if-else
-  } else if(matchkey(opt, "pull_nstfout") || matchkey(opt, "pull-nstfout")) {
+  } else if(nocmmatchkey(opt, "pull_nstfout") || nocmmatchkey(opt, "pull-nstfout")) {
     setsc(optval, nstf);
-  } else if(matchkey(opt, "pull_ngroups") || matchkey(opt, "pull-ngroups")) {
+  } else if(nocmmatchkey(opt, "pull_ngroups") || nocmmatchkey(opt, "pull-ngroups")) {
     setsc(optval, npgrps);
     pullgrps.resize(npgrps);
-  } else if(matchkey(opt, "pull_group0") || matchkey(opt, "pull-group0")) {
+  } else if(nocmmatchkey(opt, "pull_ncontactgroups") || nocmmatchkey(opt, "pull-ncontactgroups")) {
+    setsc(optval, ncntgrps);
+    pullcntgrps.resize(ncntgrps);
+  } else if(nocmmatchkey(opt, "pull_group0") || nocmmatchkey(opt, "pull-group0")) {
     return true;
   } else {
-    for(uint i = 0; i < npgrps; ++i) {
-      if(pullgrps[i](opt, optval, tostr(i+1))) { return true; }
+    if(pullT == Contact) {
+      for(uint i = 0; i < ncntgrps; ++i) {
+	if(pullcntgrps[i](opt, optval, tostr(i))) { return true; }
+      }
+    } else {
+      for(uint i = 0; i < npgrps; ++i) {
+        if(pullgrps[i](opt, optval, tostr(i+1))) { return true; }
+      } 
     }
     return false;
   }
@@ -334,22 +442,81 @@ GMXMDP<THamiltonian>::PULL::PULLGRP::PULLGRP() :
 }
 
 template < class THamiltonian >
+void GMXMDP<THamiltonian>::PULL::PULLGRP::print() const {
+  printf("#  %20s = ", "pull_vec");
+  copy(vec.begin(), vec.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#  %20s = ", "pull_init");
+  copy(init.begin(), init.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#  %20s = ", "pull_initB");
+  copy(initB.begin(), initB.end(), ostream_iterator<double>(cout, " "));
+  cout << endl;
+  printf("#  %20s = %-10.5lf\n", "pull_rate", rate);
+  printf("#  %20s = %-10.5lf\n", "pull_k", k);
+  printf("#  %20s = %-10.5lf\n", "pull_kB", kB);
+}
+
+template < class THamiltonian >
 bool GMXMDP<THamiltonian>::PULL::PULLGRP::operator()(const string& opt, const string& optval, const string& i) throw(GMXMDP_Exception, FILEIO_Exception)
 {
-  if(matchkey(opt, "pull_group" + i) || matchkey(opt, "pull-group" + i)) {
+  if(nocmmatchkey(opt, "pull_group" + i) || nocmmatchkey(opt, "pull-group" + i)) {
     return true; //not really need this
-  } else if(matchkey(opt, "pull_vec" + i) || matchkey(opt, "pull-vec" + i)) {
+  } else if(nocmmatchkey(opt, "pull_vec" + i) || nocmmatchkey(opt, "pull-vec" + i)) {
     setvec(optval, vec);
-  } else if(matchkey(opt, "pull_init" + i) || matchkey(opt, "pull-init" + i)) {
+  } else if(nocmmatchkey(opt, "pull_init" + i) || nocmmatchkey(opt, "pull-init" + i)) {
     setvec(optval, init);
-  } else if(matchkey(opt, "pull_initB" + i) || matchkey(opt, "pull-initB" + i)) {
+  } else if(nocmmatchkey(opt, "pull_initB" + i) || nocmmatchkey(opt, "pull-initB" + i)) {
     setvec(optval, initB);
-  } else if(matchkey(opt, "pull_rate" + i) || matchkey(opt, "pull-rate" + i)) {
+  } else if(nocmmatchkey(opt, "pull_rate" + i) || nocmmatchkey(opt, "pull-rate" + i)) {
     setsc(optval, rate);
-  } else if(matchkey(opt, "pull_k" + i) || matchkey(opt, "pull-k" + i)) {
+  } else if(nocmmatchkey(opt, "pull_k" + i) || nocmmatchkey(opt, "pull-k" + i)) {
     setsc(optval, k);
-  } else if(matchkey(opt, "pull_kB" + i) || matchkey(opt, "pull-kB" + i)) {
+  } else if(nocmmatchkey(opt, "pull_kB" + i) || nocmmatchkey(opt, "pull-kB" + i)) {
     setsc(optval, kB);
+  } else {
+    return false;
+  }
+  return true;
+}
+
+template < class THamiltonian >
+GMXMDP<THamiltonian>::PULL::PULLCNTGRP::PULLCNTGRP() :
+  rate(0.0),
+  nc(0.0),
+  ncB(0.0),
+  k(0.0),
+  kB(k)
+{
+}
+
+template < class THamiltonian >
+void GMXMDP<THamiltonian>::PULL::PULLCNTGRP::print() const {
+  printf("#  %20s = %-10.5lf\n", "rate", rate);
+  printf("#  %20s = %-10.5lf\n", "nc", nc);
+  printf("#  %20s = %-10.5lf\n", "ncB", ncB);
+  printf("#  %20s = %-10.5lf\n", "k", k);
+  printf("#  %20s = %-10.5lf\n", "kB", kB);
+}
+
+template < class THamiltonian >
+bool GMXMDP<THamiltonian>::PULL::PULLCNTGRP::operator()(const string& opt, const string& optval, const string& i) throw(GMXMDP_Exception, FILEIO_Exception)
+{
+  const string cntgrpnm = "pull_contactgroup" + i;
+  if(nocmmatchkey(opt, cntgrpnm + "_pgrp1")) {
+    return true; //not really need this
+  } else if(nocmmatchkey(opt, cntgrpnm + "_pgrp2")) {
+    return true; //not really need this
+  } else if(nocmmatchkey(opt, cntgrpnm + "_ncrate")) {
+    setsc(optval, rate);
+  } else if(nocmmatchkey(opt, cntgrpnm + "_k")) {
+    setsc(optval, k);
+  } else if(nocmmatchkey(opt, cntgrpnm + "_kB")) {
+    setsc(optval, kB);
+  } else if(nocmmatchkey(opt, cntgrpnm + "_nc")) {
+    setsc(optval, nc);
+  } else if(nocmmatchkey(opt, cntgrpnm + "_ncB")) {
+    setsc(optval, ncB);
   } else {
     return false;
   }
