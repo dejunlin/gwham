0763580b2f00a7b3266d4b12652c28a8110189c4
Dejun Lin <dejun@cole.biophysics.rochester.edu>
2014-08-11 17:35:01 -0400

    forgot to check-in a cpp in the last commit

---
+++ hamiltonian.cpp
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hamiltonian.cpp
+ *
+ *    Description:  Hamiltonian of various ensembles 
+ *
+ *        Version:  1.0
+ *        Created:  11/08/14 13:55:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dejun Lin (DL), dejun.lin@gmail.com
+ *   Organization:  Department of Biochemistry and Biophysics, Medical Center, University of Rochester
+ *
+ * =====================================================================================
+ */
+
+#include "hamiltonian.hpp"
+
+Hamiltonian::Hamiltonian(const vPFunct& _potentials) : 
+  potentials(_potentials)
+  {};
+
+Hamiltonian::Hamiltonian(const Hamiltonian& _H) :
+  potentials(_H.getPotentialFuncts())
+  {};
+
+const vPFunct& Hamiltonian::getPotentialFuncts() const {
+  return potentials;
+}
+
+valtype Hamiltonian::ener(const vector<valtype>& vals) const {
+  valtype ans = 0.0;
+  for(uint i = 0; i < vals.size(); ++i) {
+    ans += (*potentials[i])(vals[i]);
+  }
+  return ans;
+}
