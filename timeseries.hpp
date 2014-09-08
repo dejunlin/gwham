2e0b7c547fa50009184a29adb7ba405fa79f33f5
Dejun Lin <dejun.lin@gmail.com>
2014-09-07 22:33:02 -0400

    updated TimeSeries class

--- timeseries.hpp
+++ timeseries.hpp
@@ -20,42 +20,94 @@
  */
 #include "typedefs.hpp"
 #include "fileio.hpp"
+#include "fileio_utils.hpp"
 #include "exception.hpp"
 #include <vector>
-#include "column_processor.hpp"
-
+#include <functional>
+#include <limits>
 /*
  * =====================================================================================
  *        Class:  TimeSeries
  *  Description:  Interfacial time-series-file reader class
  * =====================================================================================
  */
-template < class Output >
+template < class INPUT >
 class TimeSeries
 {
   public:
+    typedef TimeSeries<INPUT> ThisType;
+    typedef std::reference_wrapper<INPUT> rINPUT;
+  protected:
+    /* ====================  METHODS       ======================================= */
+
+    /* ====================  DATA MEMBERS  ======================================= */
+    fileio fio;
+    //! Mask indicates which columns are used; 
+    const ulong Mask = 0;
+    //! Total number of columns in input (should be <= the number of digits in Mask)
+    const ulong iNcol = 0;
+    //! Total number of columns where are interested in
+    const ulong oNcol = 0;
+    vector<INPUT> vInput;
+    vector<rINPUT> vrInput;
+
+  public:
+    typedef decltype(TimeSeries<INPUT>::Mask) MaskT;
+    constexpr static int Nbit = numeric_limits<MaskT>::digits;
     /* ====================  LIFECYCLE     ======================================= */
-    TimeSeries (fileio& _fio, const vector<column_processor>& _cps) : fio(_fio), cps(_cps) {};                             /* constructor */
+    TimeSeries (fileio&& _fio, const uint _Mask, const uint _iNcol, const uint _oNcol) :
+      fio(std::move(_fio)), 
+      Mask(_Mask),
+      iNcol(_iNcol),
+      oNcol(_oNcol),
+      vInput(vector<INPUT>(iNcol))
+      { init(); };
+
+    TimeSeries (fileio& _fio, const uint _Mask, const uint _iNcol, const uint _oNcol) : 
+      fio(_fio), 
+      Mask(_Mask),
+      iNcol(_iNcol),
+      oNcol(_oNcol),
+      vInput(vector<INPUT>(iNcol))
+      { init(); };
 
     /* ====================  ACCESSORS     ======================================= */
 
     /* ====================  MUTATORS      ======================================= */
 
     /* ====================  OPERATORS     ======================================= */
-    virtual linecounter operator()(const string& fname, Output& data) = 0;
-
-  protected:
-    /* ====================  METHODS       ======================================= */
-
-    /* ====================  DATA MEMBERS  ======================================= */
-    fileio fio;
-    const vector<column_processor> cps;
+    //! Read a file and process them using the DATAPROCESSOR object
+    template < class DATAPROCESSOR >
+    linecounter operator()(const string& fname, const DATAPROCESSOR& dproc) {
+      linecounter Nl = 0;
+      if(!fio.fopen(fname)) { return Nl; }
+      while(fio.readaline()) {
+	const vector<double> cols(fio.line2val());
+	for(uint i = 0; i < vInput.size(); ++i) vInput[i] = cols[i];
+	dproc(vrInput);
+	++Nl;
+      }
+    }
 
   private:
     /* ====================  METHODS       ======================================= */
+    void init() {
+      if(iNcol > Nbit) {
+	  throw(TimeSeries_Exception("Number of column in time-series file (" + tostr(iNcol) +") exceeds the limit (" + tostr(Nbit) + ")"));
+      }
+
+      //This initialize the references to the columns in the input data
+      for(uint i = 0; i < iNcol; ++i) {
+	if(Mask & (1<<i)) {
+	  vrInput.emplace_back(vInput[i]);
+	}
+      }
+    }
 
     /* ====================  DATA MEMBERS  ======================================= */
 
 }; /* ----------  end of template class TimeSeries  ---------- */
 
+template < class INPUT>
+constexpr int TimeSeries<INPUT>::Nbit;
 #endif
