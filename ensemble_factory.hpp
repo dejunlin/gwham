7c68e8ca62db04b69db649d95e487c9a82725333
Dejun Lin <dejun@gmail.com>
2014-09-11 17:05:55 -0400

    implement a map to tell which ensembles each of the MDPs work in

--- ensemble_factory.hpp
+++ ensemble_factory.hpp
@@ -42,7 +42,7 @@ chkmdp ( const PMDP& pmdp )
 
 template < class PMDP >
 typename std::enable_if<std::is_pointer<PMDP>::value, void>::type
-genens(const PMDP& pmdp, vpEnsemble& ens, bool combinestates) {
+genens(const PMDP& pmdp, vpEnsemble& ens, multimap<PMDP, uint>& pmdp2ipens, bool combinestates) {
   //TODO: when we support expanded ensemble on other FEP parameter
   //set, we need to retrieve the corresponding vector of 
   //functors/parameters from the MDP object
@@ -69,16 +69,20 @@ genens(const PMDP& pmdp, vpEnsemble& ens, bool combinestates) {
     //if true, we'll check if this new ensemble is equivalent to 
     //the ensemble we've already created -- if yes, we skip pushing
     //it into ens
-    //TODO: in case we have redundant ensembles, we need to create 
-    //a map to tell which mdp coresponds to which ensembles
     if(combinestates) {
 	bool skip = false;
-	for(const auto& oldpens : ens) {
-	  if(*newpens == *oldpens) { skip = true; break; }
+	for(uint j = 0; j < ens.size(); ++j) {
+	  const auto& oldpens = ens[j];
+	  if(*newpens == *oldpens) {
+	    pmdp2ipens.insert(make_pair(pmdp, j));
+	    skip = true; 
+	    break; 
+	  }
 	}
 	if(skip) { continue; }
     }
     ens.emplace_back(newpens);
+    pmdp2ipens.insert(make_pair(pmdp, ens.size()-1));
   }
 }
 
@@ -110,12 +114,12 @@ void chkens(vpEnsemble& ens) {
 
 template <class PMDP>
 typename std::enable_if<std::is_pointer<PMDP>::value, vpEnsemble>::type
-MDP2Ensemble (const vector<PMDP>& pmdps, const bool combinestates)
+MDP2Ensemble (const vector<PMDP>& pmdps, multimap<PMDP, uint>& pmdp2ipens, const bool combinestates)
 {
   vpEnsemble ans;
   for(const auto& pmdp : pmdps) {
     chkmdp(pmdp);   
-    genens(pmdp, ans, combinestates);
+    genens(pmdp, ans, pmdp2ipens, combinestates);
   }
   //! Check if we need to take care of temperature and pressure
   chkens(ans);
