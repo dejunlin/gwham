6be868dae1bcb62caf83b0a73f96d363f8cf2aaa
Dejun Lin <dejun@gmail.com>
2014-09-11 17:11:07 -0400

    create ts and ensemble in gwham

--- gwham.cpp
+++ gwham.cpp
@@ -1,9 +1,3 @@
-#include "typedefs.hpp"
-#include "hamiltonian.hpp"
-#include "ensemble.hpp"
-#include "gnarray.hpp"
-#include "fileio.hpp"
-#include "fileio_utils.hpp"
 #include <limits>
 #include <iostream>
 #include <sstream>
@@ -13,15 +7,29 @@
 #include <iterator>
 #include <stdio.h>
 #include <stdlib.h>
+#include "typedefs.hpp"
+#include "hamiltonian.hpp"
+#include "ensemble.hpp"
+#include "gnarray.hpp"
+#include "fileio.hpp"
+#include "fileio_utils.hpp"
+#include "gmxmdp.hpp"
+#include "ensemble_factory.hpp"
 
 using namespace std;
 
 typedef gnarray<coordtype, valtype, valtype> narray;
 typedef gnarray<coordtype, histcounter, valtype> histogram;
 
+typedef map<string, function<pMDP(const string&)>> MDP_Factory;
+static const MDP_Factory suffix2mdp {
+  {"mdp", GMXMDP::CreateMDP},
+};
+
+
 int main(int argc, char* argv[]) {
-  string cmdline("#Usage: gwham sysname nwin nrun rcprint nbins hv lv tol rcvbegin rcvstride rcvend fseeds\n");
-  if (argc != 13) {
+  string cmdline("#Usage: gwham sysname mdpsuffixes nwin nrun rcprint nbins hv lv tol rcvbegin rcvstride rcvend fseeds\n");
+  if (argc != 14) {
     cerr << cmdline;
     exit(-1);
   }
@@ -30,7 +38,7 @@ int main(int argc, char* argv[]) {
   //(your files should be named as "sysname_WINID_RUNID*.*")
   const string sysname = string(argv[k++]); 
   //file names suffixes for each type of input files ( the number of file types depends on the system mdp setup) 
-  const vector<string> fnsuffixes = split<string>(argv[k++]); 
+  const vector<string> mdpsuffixes = split<string>(argv[k++]); 
   //# of replica 
   const uint nwin = atoi(argv[k++]);
   //# of runs performed
@@ -77,13 +85,65 @@ int main(int argc, char* argv[]) {
   if(rcvend  < rcvbegin) {
     throw(General_Exception("rcvend must be larger than rcvbegin"));
   }
+   
+  for(const auto& mdpsuffix : mdpsuffixes) {
+    const MDP_Factory::const_iterator it = suffix2mdp.find(mdpsuffix);
+    if(it == suffix2mdp.end()) {
+      throw(General_Exception("MDP file with suffix '"+mdpsuffix+"' not supported"));
+    }
+  }
 
-  const string mdpsuffix = fnsuffixes[0];
-  fnsuffixes.erase(fnsuffixes.begin());
+  //create MDP objects by reading the mdp files
+  //first suffix is for MDP file (format is sysname_winid.suffix)
   vector<pMDP> pmdps;
   for(uint i = 0; i < nwin; ++i) {
-    //first suffix is for MDP file (format is sysname_winid.suffix
-    const string fmdp = sysname+"_"+tostr(i)+"."+mdpsuffix;
+    for(const auto& mdpsuffix : mdpsuffixes) {
+      const string fmdp = sysname+"_"+tostr(i)+"."+mdpsuffix;
+      try {
+	// check if the file exists
+        fileio fio(fmdp, fstream::in, 1, 0, 1, MAXNLINE, ";#@");
+      } catch (FILEIO_Exception& fioex) {
+	continue;
+      }
+      pmdps.emplace_back( suffix2mdp.find(mdpsuffix)->second(fmdp) );
+    }
+  }
+
+  //create ensemble
+  multimap<pMDP, uint> pmdp2ipens;
+  const auto pens = MDP2Ensemble(pmdps, pmdp2ipens, 1);
+  cout << "#created " << pens.size() << " ensembles\n";
+  for(auto& pmdp : pmdps) {
+    auto itr = pmdp2ipens.equal_range(pmdp);
+    cout << "#MDP file: " << pmdp->fname << " contains to the following ensembles: ";
+    for(auto& it = itr.first; it != itr.second; ++it) {
+      cout << it->second << " ";
+    }
+    cout << endl;
+  }
+
+  //create histogram (1 for each ensemble)
+  vector<histogram> hists{pens.size(), histogram{ndim, nbins, hv, lv}};
+
+  //create the time-series readers
+  for(const auto& pmdp : pmdps) {
+    auto vts = pmdp->CreateTimeSeries();
+
+    cout << "#require " << vts.size() << " types of time-serie files for " << pmdp->fname << " : ";
+    for(const auto& ts : vts) {
+      cout << ts.fnsuffix << " ";
+    }
+    cout << endl;
+
+    const auto tsprefix = getfnfixes(pmdp->fname)[0];
+    for(auto& ts : vts) {
+      const auto tssuffix = ts.fnsuffix;
+      for(uint i = 0; i < nrun; ++i) {
+	const string fts = tsprefix + "_" + tostr(i) + tssuffix;
+        cout << "about to read file: " << fts << endl;
+      }
+    }
 
   }
+
 }
