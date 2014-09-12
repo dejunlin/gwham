862931748c52b12ebbcef5da3ad8b793f4552c31
Dejun Lin <dejun@gmail.com>
2014-09-12 17:21:12 -0400

    add a set of histogrammer function as dataprocessor in timeseries

--- gwham.cpp
+++ gwham.cpp
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <stdio.h>
 #include <stdlib.h>
+#include <functional>
 #include "typedefs.hpp"
 #include "hamiltonian.hpp"
 #include "ensemble.hpp"
@@ -15,17 +16,42 @@
 #include "fileio_utils.hpp"
 #include "gmxmdp.hpp"
 #include "ensemble_factory.hpp"
+#include "mdp_factory.hpp"
 
 using namespace std;
 
 typedef gnarray<coordtype, valtype, valtype> narray;
 typedef gnarray<coordtype, histcounter, valtype> histogram;
 
-typedef map<string, function<pMDP(const string&)>> MDP_Factory;
-static const MDP_Factory suffix2mdp {
-  {"mdp", GMXMDP::CreateMDP},
-};
+void HistogramCV(histogram& hist, uint& N, const vector<valtype>& input) {
+  if(hist.bin(input) != hist.end()) ++N; 
+}
 
+void HistogramCV(vector<histogram>& hists, uint& N, vector<uint>::const_iterator& itstate, vector<valtype>::const_iterator itpot, const vector<valtype>& input) {
+  const auto& state = *itstate;
+  auto& hist = hists[state];
+  const auto& pot = *itpot;
+  vector<valtype> data = input;
+  data.push_back(pot);
+  HistogramCV(hist, N, data);
+  ++itstate;
+  ++itpot;
+}
+
+void HistogramCV(vector<histogram>& hists, uint& N, vector<uint>::const_iterator& itstate, const vector<valtype>& input) {
+  const auto& state = *itstate;
+  auto& hist = hists[state];
+  HistogramCV(hist, N, input);
+  ++itstate;
+}
+
+void HistogramCV(histogram& hist, uint& N, vector<valtype>::const_iterator itpot, const vector<valtype>& input) {
+  const auto& pot = *itpot;
+  vector<valtype> data = input;
+  data.push_back(pot);
+  HistogramCV(hist, N, data);
+  ++itpot;
+}
 
 int main(int argc, char* argv[]) {
   string cmdline("#Usage: gwham sysname mdpsuffixes nwin nrun rcprint nbins hv lv tol rcvbegin rcvstride rcvend fseeds\n");
@@ -86,28 +112,13 @@ int main(int argc, char* argv[]) {
     throw(General_Exception("rcvend must be larger than rcvbegin"));
   }
    
-  for(const auto& mdpsuffix : mdpsuffixes) {
-    const MDP_Factory::const_iterator it = suffix2mdp.find(mdpsuffix);
-    if(it == suffix2mdp.end()) {
-      throw(General_Exception("MDP file with suffix '"+mdpsuffix+"' not supported"));
-    }
-  }
 
   //create MDP objects by reading the mdp files
-  //first suffix is for MDP file (format is sysname_winid.suffix)
-  vector<pMDP> pmdps;
+  vector<string> fnprefixes;
   for(uint i = 0; i < nwin; ++i) {
-    for(const auto& mdpsuffix : mdpsuffixes) {
-      const string fmdp = sysname+"_"+tostr(i)+"."+mdpsuffix;
-      try {
-	// check if the file exists
-        fileio fio(fmdp, fstream::in, 1, 0, 1, MAXNLINE, ";#@");
-      } catch (FILEIO_Exception& fioex) {
-	continue;
-      }
-      pmdps.emplace_back( suffix2mdp.find(mdpsuffix)->second(fmdp) );
-    }
+    fnprefixes.emplace_back(sysname+"_"+tostr(i));
   }
+  const vector<pMDP> pmdps = CreateMDPs(fnprefixes, mdpsuffixes, suffix2MDP);
 
   //create ensemble
   multimap<pMDP, uint> pmdp2ipens;
@@ -127,8 +138,9 @@ int main(int argc, char* argv[]) {
 
   //create the time-series readers
   for(const auto& pmdp : pmdps) {
+    const bool needpotential = cmpens(pens)&(1<<Ensemble::DTemperature);
     // if temperature are different among ensembles, we need to read potential energy
-    auto vts = pmdp->CreateTimeSeries(chkens(pens)&(1<<Ensemble::DTemperature));
+    auto vts = pmdp->CreateTimeSeries(needpotential);
 
     cout << "#require " << vts.size() << " types of time-serie files for " << pmdp->fname << " : ";
     for(const auto& ts : vts) {
@@ -136,15 +148,76 @@ int main(int argc, char* argv[]) {
     }
     cout << endl;
 
-    const auto tsprefix = getfnfixes(pmdp->fname)[0];
+    // we overwrite the stride here
     for(auto& ts : vts) {
+      ts.fio.lb = rcvbegin;
+      ts.fio.ls *= rcvstride;
+      ts.fio.le = rcvend;
+    }
+
+    const auto tsprefix = getfnfixes(pmdp->fname)[0];
+    decltype(vts)::iterator it = vts.begin();
+    // For exapnded ensemble, we need to read the state time-series first
+    vector<uint> states;
+    auto itstate = states.begin(); 
+    if(pmdp->isExpandedEnsemble()) {
+      // state time-series is always the first one
+      auto& ts = *it;
       const auto tssuffix = ts.fnsuffix;
       for(uint i = 0; i < nrun; ++i) {
 	const string fts = tsprefix + "_" + tostr(i) + tssuffix;
-        cout << "about to read file: " << fts << endl;
+	ts(fts, [&states] (const vector<valtype>& input) 
+	        {
+		  states.emplace_back(input.front());
+		}
+          );
+      }
+      ++it;
+    }
+
+    vector<valtype> pot;
+    auto itpot = pot.begin();
+    if(needpotential) {
+      // potential energy time-series 
+      auto& ts = *it;
+      const auto tssuffix = ts.fnsuffix;
+      for(uint i = 0; i < nrun; ++i) {
+	const string fts = tsprefix + "_" + tostr(i) + tssuffix;
+	ts(fts, [&pot] (const vector<valtype>& input) 
+	        {
+		  pot.emplace_back(input.front());
+		}
+          );
+      }
+      ++it;
+    }
+    
+    //finally we histogram the cv; we first build a histogrammer depends 
+    //on whether we are doing expanded ensemble and/or if we need to 
+    //deal with potential energy
+    function<void(const vector<valtype>& input)> cvhistogrammer;
+    using namespace std::placeholders;
+    uint Nsamples = 0;
+    if(states.size()) {
+      if(pot.size()) {
+	cvhistogrammer = std::bind(static_cast<void(*)(vector<histogram>&, uint&, vector<uint>::const_iterator&, vector<valtype>::const_iterator, const vector<valtype>&)>(&HistogramCV), ref(hists), ref(Nsamples), ref(itstate), ref(itpot), _1); 
+      } else {
+	cvhistogrammer = std::bind(&HistogramCV, ref(hists), ref(Nsamples), ref(itstate), _1); 
+      }
+    } else {
+      const uint ihist = pmdp2ipens.find(pmdp)->second;
+      if(pot.size()) {
+	cvhistogrammer = std::bind(&HistogramCV, ref(hists[ihist]), ref(Nsamples), ref(itpot), _1); 
+      } else {
+	cvhistogrammer = std::bind(&HistogramCV, ref(hists[ihist]), ref(Nsamples), _1); 
       }
     }
 
+    auto& ts = *it;
+    const auto tssuffix = ts.fnsuffix;
+    for(uint i = 0; i < nrun; ++i) {
+      const string fts = tsprefix + "_" + tostr(i) + tssuffix;
+      ts(fts, cvhistogrammer); 
+    }
   }
-
 }
