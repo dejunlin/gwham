0f3e17105cf848059f1b413aa06d1e3702f71f9c
Dejun Lin <dejun.lin@gmail.com>
2014-12-17 16:29:43 -0500

    update CMakeLists for using cmake 3.1.0 and rewrite all the build script

--- build_debug.sh
+++ build_debug.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-export CC="gcc"
-export CXX="g++"
-cmake .. -DCMAKE_BUILD_TYPE=Debug 
+#!/bin/tcsh
+setenv PREFIX "/usr/local"
+setenv LIBDIR "$PREFIX/lib"
+setenv LIB64DIR "$PREFIX/lib64"
+setenv CC "$PREFIX/bin/gcc"
+setenv CXX "$PREFIX/bin/g++"
+setenv CPPFLAGS "-I$PREFIX/include"
+setenv LDFLAGS "-L$LIBDIR -L$LIB64DIR"
+cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=Debug 
