30eeebd984842036a2dc2bb934dccd043c0d2320
Dejun Lin <dejun.lin@gmail.com>
2016-05-24 11:56:58 -0700

    remove explicit path for gcc and g++

--- build_mpreal_release.sh
+++ build_mpreal_release.sh
@@ -2,8 +2,8 @@
 setenv PREFIX "/usr/local"
 setenv LIBDIR "$PREFIX/lib"
 setenv LIB64DIR "$PREFIX/lib64"
-setenv CC "$PREFIX/bin/gcc"
-setenv CXX "$PREFIX/bin/g++"
+setenv CC "gcc"
+setenv CXX "g++"
 setenv CPPFLAGS "-I$PREFIX/include"
 setenv LDFLAGS "-L$LIBDIR -L$LIB64DIR"
 cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=Release -DMPREALCXX=20
